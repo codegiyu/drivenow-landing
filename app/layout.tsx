4f1482b5d01d6b8ef0c0efcfe91fa8c77890aacc
Edward-Precious Omegbu <eomegbu@gmail.com>
2026-04-16 19:47:48 +0100

    fix: icons, image and author in use in seo setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,12 @@ export const metadata: Metadata = {
   title: SEO_DETAILS.title,
   description: SEO_DETAILS.description,
   alternates: SEO_DETAILS.alternates,
+  authors: SEO_DETAILS.authors,
+  icons: {
+    icon: SEO_DETAILS.icons,
+    shortcut: SEO_DETAILS.icons,
+    apple: SEO_DETAILS.icons,
+  },
   robots: SEO_DETAILS.robots,
   keywords: SEO_DETAILS.keywords,
   generator: SEO_DETAILS.generator,
@@ -38,12 +44,18 @@ export const metadata: Metadata = {
     description: SEO_DETAILS.ogDesc,
     url: SEO_DETAILS.metadataBase,
     siteName: 'DriveNow',
+    images: [
+      {
+        url: SEO_DETAILS.image,
+      },
+    ],
     type: 'website',
   },
   twitter: {
     card: 'summary_large_image',
     title: 'DriveNow',
     description: SEO_DETAILS.description,
+    images: [SEO_DETAILS.image],
   },
 };
 
